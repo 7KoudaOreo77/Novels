b4388e6bd41468f12603eee60b7842c041b396c2
EC2 Default User <ec2-user@ip-172-31-37-35.ec2.internal>
2022-09-11 13:40:08 +0000

    [Add]ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,21 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
+  def self.guest
+    find_or_create_by!(email: 'aaa@aaa.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.name = 'サンプル'
+
+    end
+  end
+
   def get_profile_image(width, height)
-   unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-   end
-   profile_image.variant(resize_to_limit: [width, height]).processed
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,48 @@
 Rails.application.routes.draw do
- devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
- }
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
- devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
- }
-
- root to: "public/homes#top"
- get 'home/about' => 'public/homes#about', as: "about"
-
- namespace :admin do
-  get 'admin/homes/top' => 'homes#top'
-
-  resources :users, only: [:index, :show, :edit, :update] do
-   member do
-    get "withdrawal" => "users#withdrawal"
-    patch "out" => "users#out"
-   end
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :novel_comments, only: [:index, :create, :destroy]
-  resources :tags, only: [:index, :create, :edit, :update, :destroy]
- end
 
- namespace :public do
-  resources :novels, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
-   resource :favorites, only: [:create, :destroy]
-   resources :tags, only: [:index, :create, :edit, :update, :destroy, :show]
-    get 'tag_search'
-   resources :novel_comments, only: [:create, :destroy]
-   collection do
-      get 'search'
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  root to: "public/homes#top"
+  get 'home/about' => 'public/homes#about', as: "about"
+
+  namespace :admin do
+    get 'admin/homes/top' => 'homes#top'
+
+    resources :users, only: [:index, :show, :edit, :update] do
+      member do
+        get "withdrawal" => "users#withdrawal"
+        patch "out" => "users#out"
+      end
+    end
+    resources :novel_comments, only: [:index, :create, :destroy]
+    resources :tags, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  namespace :public do
+    resources :novels, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :tags, only: [:index, :create, :edit, :update, :destroy, :show]
+      get 'tag_search'
+      resources :novel_comments, only: [:create, :destroy]
+      collection do
+        get 'search'
+      end
+    end
+    resources :users, only: [:index, :show, :edit, :update] do
+      member do
+        get :favorites
+      end
     end
   end
-  resources :users, only: [:index, :show, :edit, :update] do
-   member do
-    get :favorites
-   end
-  end
- end
 
 end
