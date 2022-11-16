9bad928e0de6458ac3f1dbef4d97cf1c81acd78d
EC2 Default User <ec2-user@ip-172-31-37-35.ec2.internal>
2022-11-16 13:13:38 +0000

    []

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Novels
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    
+
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
